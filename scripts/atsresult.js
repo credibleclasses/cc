34f89aad4344f73f59f5861f85e891b4c0993d0a
credibleclasses <141504537+credibleclasses@users.noreply.github.com>
2023-08-09 23:17:31 +0530

    Update atsresult.js

--- scripts/atsresult.js
+++ scripts/atsresult.js
@@ -1,16 +1,36 @@
-localStorage.getItem('roll')
-
-const atsresultroll = localStorage.getItem('roll');
 const atsresultdate = document.getElementById("atsresultdate");
-const atsresultsearchButton = document.getElementById("atsresultsearch"); // Renamed the button element to avoid naming conflict with the function
-const atserror = document.getElementById("atsresulterror")
+const atsresultsearchButton = document.getElementById("atsresultsearch");
+const atserror = document.getElementById("atsresulterror");
+const token = localStorage.getItem('token');
+const apiLink = process.env.API_LINK;
+async function verifyToken() {
+    try {
+        const response = await fetch('https://server-as2k.onrender.com/verify', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        const userData = await response.json();
 
+        if (response.ok) {
+            return userData.roll;
+        } else {
+            console.error('Token verification failed:', userData);
+            throw new Error('Token verification failed');
+        }
+    } catch (error) {
+        console.error('Error verifying token:', error);
+        throw error;
+    }
+}
 
 async function atsresultsearch() {
     atsresultsearchButton.innerText = "Searching..";
     atsresultsearchButton.disabled = true;
-
+console.log("Date ",atsresultdate)
     try {
+        const atsresultroll = await verifyToken();
         const response = await fetch(`https://server-as2k.onrender.com/getresult/ats/${atsresultroll}/${atsresultdate.value}`, {
             method: 'POST',
             headers: {
@@ -18,46 +38,47 @@ async function atsresultsearch() {
             }
         });
         const data = await response.json();
-        console.log(data);
-        
+
         if (response.ok) {
-            atserror.classList.add("none")
-            const showatsresult = document.getElementById("atsresultdisplay")
+            console.log(data.Exam_Date)
+            console.log(data.Exam_Type)
+            atserror.classList.add("none");
+            const showatsresult = document.getElementById("atsresultdisplay");
             showatsresult.innerHTML = `
-            <div class="container mt-3 mb-5">
-            <h3 class="text-center bold bottom-border mb-2 ">Your Result</h3>
-            <div class="table-responsive">
-                <table class="table table-bordered">
-                    <tbody>
-                        <tr>
-                            <td class="bold" >Exam Type</td>
-                            <td>${data.Exam_Type}</td>
-                        </tr>
-                        <tr>
-                            <td class="bold" >Exam Date</td>
-                            <td>${data.Exam_Date}</td>
-                        </tr>
-                        <tr>
-                            <td class="bold" >Full Marks</td>
-                            <td>${data.Full_Marks}</td>
-                        </tr>
-                        <tr>
-                            <td class="bold" >Obtained Marks</td>
-                            <td>${data.Obtained_Marks}</td>
-                        </tr>
-                        <tr>
-                            <td class="bold" >Percentage</td>
-                            <td>${(data.Obtained_Marks/data.Full_Marks)*100}%</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            </div>`;
-        }
-        else{
-            atserror.innerHTML = ` <div class="alert alert-danger alert-dismissible fade show" role="alert" id="incorrectalert">
-            <strong>Not Found!</strong> Exam Date is incorrect.
-          </div>`
+        <div class="container mt-3 mb-5">
+          <h3 class="text-center bold bottom-border mb-2 ">Your Result</h3>
+          <div class="table-responsive">
+            <table class="table table-bordered">
+              <tbody>
+                <tr>
+                  <td class="bold">Exam Type</td>
+                  <td>${data.Exam_Type}</td>
+                </tr>
+                <tr>
+                  <td class="bold" >Exam Date</td>
+                  <td>${data.Exam_Date}</td>
+                </tr>
+                <tr>
+                  <td class="bold" >Full Marks</td>
+                  <td>${data.Full_Marks}</td>
+                </tr>
+                <tr>
+                  <td class="bold" >Obtained Marks</td>
+                  <td>${data.Obtained_Marks}</td>
+                </tr>
+                <tr>
+                  <td class="bold" >Percentage</td>
+                  <td>${(data.Obtained_Marks / data.Full_Marks) * 100}%</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>`;
+        } else {
+            atserror.innerHTML = `
+        <div class="alert alert-danger alert-dismissible fade show" role="alert" id="incorrectalert">
+          <strong>Not Found!</strong> Exam Date is incorrect.
+        </div>`;
         }
     } catch (error) {
         console.error("Error:", error);
