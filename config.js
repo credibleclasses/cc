a2a356ed80d301797bfe6cb28c85a112fef21bf3
credibleclasses <141504537+credibleclasses@users.noreply.github.com>
2023-10-27 15:00:48 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BGMI CONFIG</title>
+
+    <!-- Include Bootstrap CSS and JavaScript -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">BGMI CONFIG 2.8</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<p style="background-color: white; border: 0px; box-sizing: inherit; color: #3f3f46; font-family: &quot;Open Sans&quot;, serif; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 1.4rem; font-stretch: inherit; font-variant-alternates: inherit; font-variant-east-asian: inherit; font-variant-numeric: inherit; font-variant-position: inherit; font-variation-settings: inherit; line-height: 1.8; margin: 0px 0px 1.5em; outline: 0px; padding: 0px; vertical-align: baseline; word-break: break-word;"><span style="border: 0px; box-sizing: inherit; font-family: inherit; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 14px; font-stretch: inherit; font-style: inherit; font-variant: inherit; font-variation-settings: inherit; font-weight: 600; line-height: inherit; margin: 0px; outline: 0px; padding: 0px; vertical-align: baseline;">
+
+Bgmi 2.8 90 Fps Config File Download</span>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6044316283536287"
+     crossorigin="anonymous"></script>
+<!-- test1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6044316283536287"
+     data-ad-slot="8810052486"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+&nbsp;: So friends, do you guys play BGMI game then this article is going to be very helpful for all of you. Because inside this article you are going to get the config file of all BGMI games. So that your BGMI game will run smoothly and smoothly. In which all of you also get the support of no recoil. So if you guys want to get the BGMI 90 fps config file, then read this article carefully and understand that you will understand everything here that how you can play a good game by storing the config file in your BGMI game. And the config file that you are going to get inside this article, you will get the config file of the latest version 2.8 version of BGMI game.
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6044316283536287"
+     crossorigin="anonymous"></script>
+<!-- test1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6044316283536287"
+     data-ad-slot="8810052486"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+</p><figure class="wp-block-image size-large" style="background-color: white; box-sizing: inherit; color: #3f3f46; font-family: &quot;Open Sans&quot;, serif; font-size: 16px; margin: 0px 0px 1em; text-align: center;"><img alt="" class="wp-image-1287" decoding="async" height="576" sizes="(max-width: 1024px) 100vw, 1024px" src="https://www.newsdedo.in/wp-content/uploads/2023/08/lhxpgcuj75k-hd3601748271038490093-1024x576.jpg" srcset="https://www.newsdedo.in/wp-content/uploads/2023/08/lhxpgcuj75k-hd3601748271038490093-1024x576.jpg 1024w, https://www.newsdedo.in/wp-content/uploads/2023/08/lhxpgcuj75k-hd3601748271038490093-300x169.jpg 300w, https://www.newsdedo.in/wp-content/uploads/2023/08/lhxpgcuj75k-hd3601748271038490093-768x432.jpg 768w, https://www.newsdedo.in/wp-content/uploads/2023/08/lhxpgcuj75k-hd3601748271038490093.jpg 1280w" style="box-sizing: border-box; height: auto; max-width: 100%; vertical-align: bottom;" width="1024" /><figcaption class="wp-element-caption" style="box-sizing: inherit; color: #555555; font-size: 13px; margin-bottom: 1em; margin-top: 0.5em;">Bgmi 90 Fps Config File Download</figcaption></figure><div class="block-title" style="background-color: white; border: 0px; box-sizing: inherit; color: #3f3f46; font-family: &quot;Open Sans&quot;, serif; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 16px; font-stretch: inherit; font-variant-alternates: inherit; font-variant-east-asian: inherit; font-variant-numeric: inherit; font-variant-position: inherit; font-variation-settings: inherit; line-height: inherit; margin: 0px; outline: 0px; padding: 0px; vertical-align: baseline;"><h4 class="wp-block-heading" style="border: 0px; box-sizing: inherit; color: #27272a; font-family: &quot;Open Sans&quot;, sans-serif; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 2rem; font-stretch: inherit; font-style: inherit; font-variant: inherit; font-variation-settings: inherit; font-weight: 500; line-height: 1.3; margin: 0px 0px 20px; outline: 0px; padding: 0px; vertical-align: baseline;"><span style="border: 0px; box-sizing: inherit; font-family: inherit; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 20px; font-stretch: inherit; font-style: inherit; font-variant: inherit; font-variation-settings: inherit; font-weight: 600; line-height: inherit; margin: 0px; outline: 0px; padding: 0px; vertical-align: baseline;">How to download Bgmi 90 Fps Config file</span>&nbsp;<span style="border: 0px; box-sizing: inherit; font-family: inherit; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 20px; font-stretch: inherit; font-style: inherit; font-variant: inherit; font-variation-settings: inherit; font-weight: 600; line-height: inherit; margin: 0px; outline: 0px; padding: 0px; vertical-align: baseline;">2.8</span></h4></div><p style="background-color: white; border: 0px; box-sizing: inherit; color: #3f3f46; font-family: &quot;Open Sans&quot;, serif; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 1.4rem; font-stretch: inherit; font-variant-alternates: inherit; font-variant-east-asian: inherit; font-variant-numeric: inherit; font-variant-position: inherit; font-variation-settings: inherit; line-height: 1.8; margin: 0px 0px 1.5em; outline: 0px; padding: 0px; vertical-align: baseline; word-break: break-word;">
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6044316283536287"
+     crossorigin="anonymous"></script>
+<!-- test1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6044316283536287"
+     data-ad-slot="8810052486"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+And friends, tell all of you that we are not taking any charge from all of you for the config file of this bgmi game that you are going to get inside this article. This config file is being provided absolutely free of cost to all of you. So how do you download this config file? We tell you all inside this article. So read this article carefully from here. You all will understand how you can download this config file from here. That too the config file of the latest version 2.8.</p><div class="block-title" style="background-color: white; border: 0px; box-sizing: inherit; color: #3f3f46; font-family: &quot;Open Sans&quot;, serif; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 16px; font-stretch: inherit; font-variant-alternates: inherit; font-variant-east-asian: inherit; font-variant-numeric: inherit; font-variant-position: inherit; font-variation-settings: inherit; line-height: inherit; margin: 0px; outline: 0px; padding: 0px; vertical-align: baseline;"><h4 class="wp-block-heading" style="border: 0px; box-sizing: inherit; color: #27272a; font-family: &quot;Open Sans&quot;, sans-serif; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 2rem; font-stretch: inherit; font-style: inherit; font-variant: inherit; font-variation-settings: inherit; font-weight: 500; line-height: 1.3; margin: 0px 0px 20px; outline: 0px; padding: 0px; vertical-align: baseline;"><span style="border: 0px; box-sizing: inherit; font-family: inherit; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 20px; font-stretch: inherit; font-style: inherit; font-variant: inherit; font-variation-settings: inherit; font-weight: 600; line-height: inherit; margin: 0px; outline: 0px; padding: 0px; vertical-align: baseline;">Download Bgmi 90 Fps Config file 2.8 Latest Version</span></h4></div><p style="background-color: white; border: 0px; box-sizing: inherit; color: #3f3f46; font-family: &quot;Open Sans&quot;, serif; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 1.4rem; font-stretch: inherit; font-variant-alternates: inherit; font-variant-east-asian: inherit; font-variant-numeric: inherit; font-variant-position: inherit; font-variation-settings: inherit; line-height: 1.8; margin: 0px 0px 1.5em; outline: 0px; padding: 0px; vertical-align: baseline; word-break: break-word;">So now finally friends, here we are going to tell you how you can download the config file of the latest version 2.8 of your BGMI game.
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6044316283536287"
+     crossorigin="anonymous"></script>
+<!-- test1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6044316283536287"
+     data-ad-slot="8810052486"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+So what you need to do first to download this config file If you look at the bottom side of this article, you will be able to see a button here (Download Now), then what you have to do there is to click on that (Download Now) button. Now like you Only if you click there, another article will open in front of all of you. So you have to scroll down that article slowly and go to the bottom side, then there also you get to see a button of Download Now. After that, if you click there, then your 60-second countdown will start there, after that, as soon as the countdown is over, you will get to see a button there (download file). So click there as you like. If you do this, the config file of your BGMI game latest version 2.8 will be downloaded.</p><center style="background-color: white; box-sizing: inherit; color: #3f3f46; font-family: &quot;Open Sans&quot;, serif; font-size: 16px;">
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6044316283536287"
+     crossorigin="anonymous"></script>
+<!-- test1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6044316283536287"
+     data-ad-slot="8810052486"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<a id="download-btn" href="https://telegram.me/+8RTWHdop4z0zODVl" style="background: dodgerblue; border-color: initial; border-radius: 5px; border-style: none; border-width: initial; box-sizing: inherit; color: white; cursor: pointer; font-family: &quot;Open Sans&quot;, serif; font-size: 16px; font-weight: 600; line-height: 1.8; overflow-wrap: break-word; padding: 10px 25px; transition: background-color 0.3s linear 0s;"><span class="fa fa-download" style="-webkit-font-smoothing: antialiased; box-sizing: inherit; color: #71717a; display: inline-block; font-family: FontAwesome; font-feature-settings: normal; font-kerning: auto; font-optical-sizing: auto; font-size: inherit; font-stretch: normal; font-variant-alternates: normal; font-variant-east-asian: normal; font-variant-numeric: normal; font-variant-position: normal; font-variation-settings: normal; font-weight: normal; line-height: 1; text-rendering: auto;"></span>&nbsp;Download Now</a></center>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6044316283536287"
+     crossorigin="anonymous"></script>
+<!-- test1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6044316283536287"
+     data-ad-slot="8810052486"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+
+
+<center style="background-color: white; box-sizing: inherit; color: #3f3f46; font-family: &quot;Open Sans&quot;, serif; font-size: 16px;"></center><div class="block-title" style="background-color: white; border: 0px; box-sizing: inherit; color: #3f3f46; font-family: &quot;Open Sans&quot;, serif; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 16px; font-stretch: inherit; font-variant-alternates: inherit; font-variant-east-asian: inherit; font-variant-numeric: inherit; font-variant-position: inherit; font-variation-settings: inherit; line-height: inherit; margin: 0px; outline: 0px; padding: 0px; vertical-align: baseline;"><h5 class="wp-block-heading" style="border: 0px; box-sizing: inherit; color: #27272a; font-family: &quot;Open Sans&quot;, sans-serif; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 1.8rem; font-stretch: inherit; font-style: inherit; font-variant: inherit; font-variation-settings: inherit; font-weight: 500; line-height: 1.3; margin: 0px 0px 20px; outline: 0px; padding: 0px; vertical-align: baseline;"><span style="border: 0px; box-sizing: inherit; font-family: inherit; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 18px; font-stretch: inherit; font-style: inherit; font-variant: inherit; font-variation-settings: inherit; font-weight: 600; line-height: inherit; margin: 0px; outline: 0px; padding: 0px; vertical-align: baseline;">Conclusion Result</span></h5></div><p style="background-color: white; border: 0px; box-sizing: inherit; color: #3f3f46; font-family: &quot;Open Sans&quot;, serif; font-feature-settings: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-size: 1.4rem; font-stretch: inherit; font-variant-alternates: inherit; font-variant-east-asian: inherit; font-variant-numeric: inherit; font-variant-position: inherit; font-variation-settings: inherit; line-height: 1.8; margin: 0px; outline: 0px; padding: 0px; vertical-align: baseline; word-break: break-word;">
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6044316283536287"
+     crossorigin="anonymous"></script>
+<!-- test1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6044316283536287"
+     data-ad-slot="8810052486"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+So we hope that you guys will be able to easily download the config file of the latest version 2.8 of this BGMI game very easily. But still, if you are facing any problem, then you can comment us below. Do tell us. We will give you the solution within 24 hours and you must also share this article with your friends so that they too can store the config file inside this bgmi game. Thank you.</p>
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6044316283536287"
+     crossorigin="anonymous"></script>
+<!-- test1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6044316283536287"
+     data-ad-slot="8810052486"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
